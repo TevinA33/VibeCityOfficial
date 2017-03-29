28383c4f51d897cf466f6fdde7833f961b0065ea
Tevin Adams <tevina@email.uscupstate.edu>
2017-03-29 22:26:57 +0000

    completed login

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :microposts,          only: [:create, :destroy]
+
   
   
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321002158) do
+ActiveRecord::Schema.define(version: 20170328202533) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170321002158) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
